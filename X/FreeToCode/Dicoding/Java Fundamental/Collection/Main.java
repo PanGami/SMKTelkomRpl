8c0b1b4d6d4523f228d35fda0ca9c88442111bf4
PanGami <65394405+PanGami@users.noreply.github.com>
2021-04-04 10:50:39 +0700

    Add files via upload

---
+++ X/FreeToCode/Dicoding/Java Fundamental/Collection/Main.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.collection;
+ 
+//Codelab COLLETION !
+
+import java.util.Collection;
+public class Main {
+ 
+    public static void main(String[] args) {
+        Collection collection;
+    }
+}
