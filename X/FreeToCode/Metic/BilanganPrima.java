993994fc102a81485a3818bcb2d81affb1bd31d3
PanGami <65394405+PanGami@users.noreply.github.com>
2021-10-20 08:40:41 +0700

    update Bilangan Prima

--- X/FreeToCode/Metic/BilanganPrima.java
+++ X/FreeToCode/Metic/BilanganPrima.java
@@ -1,26 +1,24 @@
-
 package Metic;
 import java.util.Scanner;
 
 public class BilanganPrima {
  
  public static void main (String args []){
-     int check;
+     String check;
      do{
-  Scanner input = new Scanner(System.in);
-  System.out.print("Silakan masukkan angka yang akan di cek: ");
+        System.out.print("Silakan masukkan angka yang akan di cek: ");   
+        Scanner input = new Scanner(System.in);
+        int angka = input.nextInt();
   
-  int angka = input.nextInt();
+        int temp;
+        boolean prima = true;
   
-  int temp;
-  boolean prima = true;
-  
-  //penggunaan for loop
-  for(int pembagi = 2; pembagi <= angka / 2; pembagi++){
-    temp = angka % pembagi;
-    if(temp == 0){
-    prima = false;
-    break;
+        //penggunaan for loop
+        for(int pembagi = 2; pembagi <= angka / 2; pembagi++){
+        temp = angka % pembagi;
+        if(temp == 0){
+        prima = false;
+        break;
    }
   }
   
@@ -29,15 +27,17 @@ public class BilanganPrima {
    bukanlah 0 dan 1
    */
   
-  if(prima && ((angka > 0)&&(angka != 1))){
-   System.out.println(angka + " adalah bilangan prima");
+  if(prima && (angka > 1)){
+   System.out.println("==>  " + angka + " adalah bilangan prima");
   }else{
-   System.out.println(angka + " bukanlah bilangan prima");
+   System.out.println("==>  " + angka + " bukanlah bilangan prima");
   }
   
-    System.out.println("Apakah anda ingin lanjut? /n jika iya ketik 1 ");
-    check = input.nextInt();
-  }while(check == 1);
+    System.out.print("Apakah anda ingin lanjut? jika iya ketik y : ");
+    check = input.next();
+    System.out.println("=================================");
+  }while(check.equalsIgnoreCase("y"));
+  
  }
 
 }
