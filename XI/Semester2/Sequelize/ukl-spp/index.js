c422bf48d1d41938ed5a4091a93c61da7ef6ff84
Panca Nugraha Wicaksana <pancanugrahawicaksana@gmail.com>
2022-03-20 19:43:28 +0700

    Pembayaran & Siswa still in development

--- XI/Semester2/Sequelize/ukl-spp/index.js
+++ XI/Semester2/Sequelize/ukl-spp/index.js
@@ -14,13 +14,17 @@ app.use("/petugas", petugas)
 const spp = require('./routes/spp');
 app.use("/spp", spp)
 
-//endpoint siswa
+//endpoint siswa >>>>>>>>>NOT YET
 const siswa = require('./routes/siswa');
 app.use("/siswa", siswa)
 
 //endpoint kelas
 const kelas = require('./routes/kelas');
 app.use("/kelas", kelas)
+
+//endpoint pembayaran >>>>>>>>>NOT YET
+const pembayaran = require('./routes/pembayaran');
+app.use("/pembayaran", pembayaran)
 
 //run server
 app.listen(8080, () => {
