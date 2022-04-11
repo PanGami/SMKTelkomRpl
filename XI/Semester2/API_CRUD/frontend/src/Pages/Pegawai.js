dda40c4881fc38b849367eff4c40b8c3af079914
Panca Nugraha Wicaksana <pancanugrahawicaksana@gmail.com>
2022-04-11 10:26:59 +0700

    Done

--- XI/Semester2/API_CRUD/frontend/src/Pages/Pegawai.js
+++ XI/Semester2/API_CRUD/frontend/src/Pages/Pegawai.js
@@ -3,21 +3,27 @@ import $ from "jquery";
 import axios from "axios";
 
 class Pegawai extends Component {
+  //make open arrow function
+  // open = () =>{
+  //   $('#modal_pegawai').modal('show');
+  // }
 
-  bind = (event) => {
-    this.setState({[event.target.name]: event.target.value});
-  }
+  // close = () => {
+  //   $("#modal_pegawai").hide();
+  // };
 
-  Add = () => {
+  add = () => {
     // mengosongkan isi variabel nip, nama, dan alamat
     // set action menjadi "insert"
+    $("#modal_pegawai").show();
+    // this.open();
     this.setState({
       nip: "",
       nama: "",
       alamat: "",
-      action: "insert"
+      action: "insert",
     });
-  }
+  };
 
   Edit = (item) => {
     /*
@@ -25,46 +31,49 @@ class Pegawai extends Component {
     akan diedit
     - set action menjadi "update"
     */
+    $("#modal_pegawai").show();
     this.setState({
       nip: item.nip,
       nama: item.nama,
       alamat: item.alamat,
-      action: "update"
+      action: "update",
     });
-  }
+  };
 
   getPegawai = () => {
     let url = "http://localhost:2910/pegawai";
     // mengakses api untuk mengambil data pegawai
-    axios.get(url)
-    .then(response => {
-      // mengisikan data dari respon API ke array pegawai
-      this.setState({pegawai: response.data.pegawai});
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
+    axios
+      .get(url)
+      .then((response) => {
+        // mengisikan data dari respon API ke array pegawai
+        this.setState({ pegawai: response.data.pegawai });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   findPegawai = (event) => {
     let url = "http://localhost:2910/pegawai";
     if (event.keyCode === 13) {
       // menampung data keyword pencarian
       let form = {
-        find: this.state.search
-      }
+        find: this.state.search,
+      };
       // mengakses api untuk mengambil data pegawai
       // berdasarkan keyword
-      axios.post(url, form)
-      .then(response => {
-        // mengisikan data dari respon API ke array pegawai
-        this.setState({pegawai: response.data.pegawai});
-      })
-      .catch(error => {
-        console.log(error);
-      });
+      axios
+        .post(url, form)
+        .then((response) => {
+          // mengisikan data dari respon API ke array pegawai
+          this.setState({ pegawai: response.data.pegawai });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }
+  };
 
   SavePegawai = (event) => {
     event.preventDefault();
@@ -72,52 +81,52 @@ class Pegawai extends Component {
     ke dalam FormData() untuk dikirim  */
     let url = "";
     if (this.state.action === "insert") {
-      url = "http://localhost:2910/pegawai/save"
+      url = "http://localhost:2910/pegawai/save";
     } else {
-      url = "http://localhost:2910/pegawai/update"
+      url = "http://localhost:2910/pegawai/update";
     }
-    
 
     let form = {
       nip: this.state.nip,
       nama: this.state.nama,
-      alamat: this.state.alamat
-    }
+      alamat: this.state.alamat,
+    };
 
     // mengirim data ke API untuk disimpan pada database
-    axios.post(url, form)
-    .then(response => {
-      // jika proses simpan berhasil, memanggil data yang terbaru
-      this.getPegawai();
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    axios
+      .post(url, form)
+      .then((response) => {
+        // jika proses simpan berhasil, memanggil data yang terbaru
+        this.getPegawai();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     // menutup form modal
-    $("#modal").modal('hide');
-  }
+    $("#modal_pegawai").modal("hide");
+  };
 
   Drop = (nip) => {
     let url = "http://localhost:2910/pegawai/" + nip;
     // memanggil url API untuk menghapus data pada database
-    if (window.confirm('Apakah Anda yakin ingin menghapus data ini?')) {
-      axios.delete(url)
-      .then(response => {
-        // jika proses hapus data berhasil, memanggil data yang terbaru
-        this.getPegawai();
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    if (window.confirm("Apakah Anda yakin ingin menghapus data ini?")) {
+      axios
+        .delete(url)
+        .then((response) => {
+          // jika proses hapus data berhasil, memanggil data yang terbaru
+          this.getPegawai();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     // method yang pertama kali dipanggil pada saat load page
-    this.getPegawai()
+    this.getPegawai();
   }
 
-   
   constructor() {
     super();
     this.state = {
@@ -165,7 +174,7 @@ class Pegawai extends Component {
                       <button
                         className="btn btn-sm btn-info m-1"
                         data-toggle="modal"
-                        data-target="#modal"
+                        data-target="#modal_pegawai"
                         onClick={() => this.Edit(item)}
                       >
                         Edit
@@ -182,19 +191,19 @@ class Pegawai extends Component {
               })}
             </tbody>
           </table>
-          <button
-            className="btn btn-success"
-            onClick={this.Add}
-            data-toggle="modal"
-            data-target="#modal"
-          >
+          <button className="btn btn-success" onClick={() => this.add()}>
             Tambah Data
           </button>
           {/* modal form pegawai */}
-          <div className="modal fade" id="modal">
+          <div className="modal" id="modal_pegawai">
             <div className="modal-dialog">
               <div className="modal-content">
-                <div className="modal-header">Form Pegawai</div>
+                <div className="modal-header">
+                  <h5>Form Pegawai</h5>
+                  {/* <button className="btn btn-danger" onClick={() => this.close()}>
+                    X
+                  </button> */}
+                </div>
                 <form onSubmit={this.SavePegawai}>
                   <div className="modal-body">
                     NIP
