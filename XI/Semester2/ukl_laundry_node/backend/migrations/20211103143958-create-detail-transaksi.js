f27b9c9a45e6da52525bbca3bbc8583b1a4f3d2b
Panca Nugraha Wicaksana <pancanugrahawicaksana@gmail.com>
2022-04-15 19:43:14 +0700

    little update

--- XI/Semester2/ukl_laundry_node/backend/migrations/20211103143958-create-detail-transaksi.js
+++ XI/Semester2/ukl_laundry_node/backend/migrations/20211103143958-create-detail-transaksi.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('tb_detail_transaksi', {
+    await queryInterface.createTable('detail_transaksi', {
       id_detail_transaksi: {
         allowNull: false,
         autoIncrement: true,
@@ -39,6 +39,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('tb_detail_transaksi');
+    await queryInterface.dropTable('detail_transaksi');
   }
 };
